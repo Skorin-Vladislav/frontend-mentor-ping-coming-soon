813be989c5a82a145ccd13b69110d57d05b078c7
Skorin Vladislav <43670004+Skorin-Vladislav@users.noreply.github.com>
2022-12-24 14:07:37 +0300

    charts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import "./App.css";
 import logo from "./img/logo.svg";
+import charts from "./img/illustration-dashboard.png";
 import Input from "./components/input-area/Input";
 
 function App() {
   return (
     <div className="App">
-      <div className="header">
-        <img className="logo" src={logo} alt="logo"></img>
-        <h1>
-          We are launching <b>soon!</b>
-        </h1>
-        <p>Subscribe and get notified</p>
-      </div>
+      <div className="container">
+        <div className="header">
+          <img className="logo" src={logo} alt="logo"></img>
+          <h1>
+            We are launching <b>soon!</b>
+          </h1>
+          <p>Subscribe and get notified</p>
+        </div>
         <Input />
+        <div className="charts">
+          <img className="dashboard" src={charts} alt="dashboard"></img>
+        </div>
+      </div>
     </div>
   );
 }
