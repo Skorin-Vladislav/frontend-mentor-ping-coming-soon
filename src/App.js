afd70d2ab3aeec00b96878a7df2ca7b94cf6655b
Skorin Vladislav <43670004+Skorin-Vladislav@users.noreply.github.com>
2022-12-24 12:41:15 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import logo from "./img/logo.svg";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="header">
+        <img className="logo" src={logo} alt="logo"></img>
+        <h1>
+          We are launching <b>soon!</b>
+        </h1>
+        <p>Subscribe and get notified</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
