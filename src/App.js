9eaf514b39d7ba1205b4ac4834d505c74840d91d
Skorin Vladislav <43670004+Skorin-Vladislav@users.noreply.github.com>
2022-12-24 14:48:25 +0300

    social media added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 import logo from "./img/logo.svg";
+import facebook from "./img/facebook-f.svg";
+import twitter from "./img/twitter.svg";
+import instagram from "./img/instagram.svg";
 import charts from "./img/illustration-dashboard.png";
 import Input from "./components/input-area/Input";
 
@@ -18,6 +21,12 @@ function App() {
         <div className="charts">
           <img className="dashboard" src={charts} alt="dashboard"></img>
         </div>
+        <div className="media">
+          <img src={facebook} alt="facebook"></img>
+          <img src={twitter} alt="twitter"></img>
+          <img src={instagram} alt="instagram"></img>
+        </div>
+        <div className="cpy-right">© Copyright Ping. All rights reserved.</div>
       </div>
     </div>
   );
